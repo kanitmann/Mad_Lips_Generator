ee29e09821a0371171f371b5461534de9ff5c285
Kanit Mann <kanitmann90@gmail.com>
2020-02-23 14:26:19 +0530

    Improve UI

--- Mad_lip_theater.py
+++ Mad_lip_theater.py
@@ -4,68 +4,85 @@ print("\n\nFill the following to generate a funny Santa-Elf Conversation: ")
 
 
 print("\nType Of Dessert")
-
-dessert = input()
+dessert = input(">>")
 print("\nSilly Word")
-silly = input()
+silly = input(">>")
 print("\nAnother Word for Father")
-father = input()
+father = input(">>")
 print("\nYear")
-year = input()
+year = input(">>")
 print("\nCelebrity Name")
 cname = input()
 print("\nType of Plant")
-plant = input()
+plant = input(">>")
 print("\nAdjective")
-adj = input()
+adj = input(">>")
 print("\nSomething You’d Name a Pet")
-pet = input()
+pet = input(">>")
 print("\nName of a Band")
-band = input()
+band = input(">>")
 print("\nPlural Animal")
-ani = input()
+ani = input(">>")
 print("\nVerb Ending in -Ing")
-verb = input()
+verb = input(">>")
 print("\nNumber")
-no = input()
+no = input(">>")
 print("\nMeasurement of Time")
-time = input()
+time = input(">>")
 print("\nBody Part")
-part = input()
+part = input(">>")
 print("\nType of Beer")
-beer = input()
+beer = input(">>")
 print("\nType of Doctor")
-dr = input()
+dr = input(">>")
 print("\nOne Syllable Noise")
-noise = input()
+noise = input(">>")
 print("\nPlural Noun")
-noun = input()
-print("\nMode of Transportation")
-trans = input()
+noun = input(">>")
+print("\nAlmost there! Just 2 more to go. \nMode of Transportation")
+trans = input(">>")
 print("\nHoliday")
-hol = input()
+hol = input(">>")
 
 
 print('''
 ELF: Hi Santa.
+
 SANTA: Hello '''+dessert +'''
+
 ELF: You’re looking at me like I’m on your naughty list.
+
 SANTA: I was counting my toys and one was ''' + silly + ''' short. I believe you stole it.
+
 ELF: But ''' + father + ''' !
+
 SANTA: I started making that toy in ''' + year + '''. What will little ''' + cname + ''' think when he/she wakes up on Christmas morning with nothing under the ''' + plant + '''?
+
 ELF: I would never steal such a ''' + adj + ''' toy.
+
 SANTA: Where were you the night before Christmas?
+
 ELF: I was with ''' + pet + '''. We were listening to ''' + band + '''’s holiday album and making snow ''' + ani + '''.
+
 SANTA: Then why did Rudolph see you ''' + verb + ''' on the roof ''' + no + ''' ''' + time + ''' before the toy went missing?
 ELF: That’s a coincidence.
+
 SANTA: The reason I work so hard, is because I get to spread joy to millions of people, and go sit by the fire shaking my ''' + part + ''' while eating cookies and drinking ''' + beer + '''.
+
 ELF: Fine, I stole the toy. But I only did it because I wanted to impress my ''' + dr + '''.
+
 SANTA: ''' + noise + ''' Every time there’s a holiday miracle, I sing my favorite Christmas song, Jingle ''' + noun + '''.
+
 (singing)
+
 Jingle ''' + noun + ''', Jingle ''' + noun + ''',
+
 Jingle all the way.
+
 Oh what fun it is to ride
+
 In a one horse open ''' + trans + '''.
+
 ELF: Merry ''' + hol + ''' Santa!''')
 
 #made by Kanit Mann
